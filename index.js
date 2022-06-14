3d5733e2d2cb2418d5f00c02eb682a22ca9d137e
ShehzadAslamOza <shehzadaslamoza@gmail.com>
2022-06-14 16:41:23 +0500

    Sign up form complete

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-let a = document.getElementsByTagName("h1")[0];
-a.style.color = "red";
+function setTheme() {
+  const root = document.documentElement;
+  const newTheme = root.className === "Dark" ? "Light" : "Dark";
+  root.className = newTheme;
+
+  document.querySelector(".theme-name").textContent = newTheme;
+}
+
+document.querySelector(".theme-toggle").addEventListener("click", setTheme);
